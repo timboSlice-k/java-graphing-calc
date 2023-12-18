6492306f0fc30657c01d348e3aede69228e844ee
timboSlice-k <timkwan44@gmail.com>
2023-12-17 18:21:09 -0800

    project created

---
+++ graphingcalc/src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.time.*;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+
+    }
+}
